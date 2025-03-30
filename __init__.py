72c880644b385b072ed45ed802b99ae30dd88e60
bstocker <boris.stocker@gmail.com>
2025-03-30 17:27:52 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,9 +5,12 @@ from flask import json
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
-@app.route('/<int:valeur>')
-def exercice(valeur):
-    return str(valeur)
+@app.route('/')
+def index():
+    user_value = request.args.get('value')
+    if not user_value:
+        return "N'oubliez pas saisir un chiffre dans votre URL"
+    return f"Valeur saisie : {user_value}"
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
