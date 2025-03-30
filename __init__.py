69488728f85844dac37476dc109127cafa5657cc
bstocker <boris.stocker@gmail.com>
2025-03-30 17:19:29 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from flask import render_template
 from flask import json
-import sqlite3
+
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
-@app.route('/')
-def hello_world():
-    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
+@app.route('/<int:valeur>')
+def exercice():
+    return valeur
 
-@app.route('/exercices/')
-def exercices():
-    return render_template('exercices.html')
+
+
+
+
+
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
