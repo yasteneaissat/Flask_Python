83aff0254979430331d52a8eea23f459e8e822ed
yasteneaissat <aissatyastene5@gmail.com>
2025-03-31 17:24:05 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/<int:valeur>')
 def exercice(valeur):
     etoiles = ''
-    for j in range(1, valeur + 1):  # Commencer de 1 à 'valeur'
-        espaces = ' ' * (valeur - j)  # Ajouter des espaces avant les étoiles pour centrer
+    for j in range(1, valeur + 1):  # On boucle de 1 à 'valeur' (le nombre de lignes)
+        espaces = ' ' * (valeur - j)  # On calcule le nombre d'espaces avant les étoiles pour centrer
         etoiles += espaces + '*' * j + '<br>'  # Ajouter les étoiles et un saut de ligne
     return etoiles
 
