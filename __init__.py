227a3fbd0899e02c90d261c89ade162455f252b5
yasteneaissat <aissatyastene5@gmail.com>
2025-04-07 14:17:19 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,28 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/<int:n>')
-def calcul_somme_securise(n):
-    somme = 0
-    nombres_ajoutes = []
-    
-    for nombre in range(1, n + 1):
-        if nombre % 11 == 0:
-            continue
-            
-        if nombre % 5 == 0 or nombre % 7 == 0:
-            # Vérifie si l'ajout dépasserait 5000
-            if somme + nombre > 5000:
-                break  # Arrêt avant dépassement
-            
-            somme += nombre
-            nombres_ajoutes.append(str(nombre))
-    
-    return (
-        f"Résultat pour n={n}<br><br>"
-        f"Nombres ajoutés: {', '.join(nombres_ajoutes)}<br><br>"
-        f"Somme finale: {somme} (garantie ≤ 5000)"
-    )
+@app.route('/<path:valeurs>')
+def exercice(valeurs):
+    liste_nombres = valeurs.split('/')
+    liste_nombres = [int(n) for n in liste_nombres]
+    resultat = 0
+    for n in liste_nombres:
+        resultat = resultat + n
+    return str(resultat)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
