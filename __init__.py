69a9b2b82e49b3f0c0a71915009b0b666f1909a8
yasteneaissat <aissatyastene5@gmail.com>
2025-04-07 10:51:34 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,28 +1,28 @@
 from flask import Flask
-from flask import render_template
-from flask import json
 
 app = Flask(__name__)
 
-@app.route('/<int:valeur>')
-def pyramide(valeur):
-    resultat = '<pre>'
-    
-    for i in range(1, valeur + 1):
-        # Créer la partie avant le sommet
-        gauche = ''.join(str(x) for x in range(1, i + 1))
-        # Créer la partie après le sommet
-        droite = ''.join(str(x) for x in range(i - 1, 0, -1))
-        
-        # Ajouter les espaces nécessaires pour centrer
-        espaces = ' ' * (valeur - i)
-        
-        # Combiner tout cela
-        resultat += espaces + gauche + droite + '\n'
-    
-    resultat += '</pre>'
-    return resultat
+# Fonction pour générer la suite de Fibonacci jusqu'à n
+def fibonacci(n):
+    fib = [0, 1]  # Initialiser la suite avec les deux premiers termes
+    for i in range(2, n):
+        fib.append(fib[i-1] + fib[i-2])  # Ajouter un terme en suivant la relation Un = Un-1 + Un-2
+    return fib
 
- 
+@app.route('/<int:n>')
+def afficher_fibonacci(n):
+    # Si n est 1, on renvoie juste [0]
+    if n == 1:
+        fib_sequence = [0]
+    else:
+        # Générer la suite de Fibonacci jusqu'à n termes
+        fib_sequence = fibonacci(n)
+    
+    # Convertir la suite en chaîne pour l'affichage
+    result = ', '.join(map(str, fib_sequence))
+    
+    # Retourner le résultat formaté
+    return f"<h1>Suite de Fibonacci jusqu'au {n}-ième terme :</h1><p>{result}</p>"
+
 if __name__ == "__main__":
     app.run(debug=True)
