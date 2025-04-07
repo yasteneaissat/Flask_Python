298fa8b32fab277ff17e139da379cd212f3f0129
yasteneaissat <aissatyastene5@gmail.com>
2025-04-07 09:38:51 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,13 +5,24 @@ from flask import json
 app = Flask(__name__)
 
 @app.route('/<int:valeur>')
-def exercice(valeur):
-    etoiles = '<pre>'
-    for j in range(1, valeur + 1):  
-        espaces = ' ' * (valeur - j)  
-        etoiles += espaces + '*' * (1 * j - 1) + '\n'
-    etoiles += '</pre>'
-    return etoiles
+def pyramide(valeur):
+    resultat = '<pre>'
+    
+    for i in range(1, valeur + 1):
+        # Créer la partie avant le sommet
+        gauche = ''.join(str(x) for x in range(1, i + 1))
+        # Créer la partie après le sommet
+        droite = ''.join(str(x) for x in range(i - 1, 0, -1))
+        
+        # Ajouter les espaces nécessaires pour centrer
+        espaces = ' ' * (valeur - i)
+        
+        # Combiner tout cela
+        resultat += espaces + gauche + droite + '\n'
+    
+    resultat += '</pre>'
+    return resultat
+
  
 if __name__ == "__main__":
     app.run(debug=True)
