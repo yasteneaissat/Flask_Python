4c713bcb9f91f54db231f6331c0293712b6d4684
yasteneaissat <aissatyastene5@gmail.com>
2025-04-07 15:19:58 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,16 +7,13 @@ def exercice(valeurs):
     # Convertir les valeurs de l'URL en une liste de nombres
     liste_nombres = valeurs.split('/')
     liste_nombres = [int(n) for n in liste_nombres]
-    
-    # Initialiser la variable max avec le premier nombre de la liste
-    max_valeur = liste_nombres[0]
-    
-    # Parcourir la liste et trouver la valeur maximale sans utiliser max()
-    for n in liste_nombres:
-        if n > max_valeur:
-            max_valeur = n
-    
-    return str(max_valeur)
+
+    # On initialise la première valeur comme valeur minimale
+    for i in range(len(liste_nombres) - 1):
+        if liste_nombres[i] < liste_nombres[i + 1]:
+            liste_nombres[i + 1] = liste_nombres[i]
+
+    return f"Le nombre minimum est : {liste_nombres[-1]}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
